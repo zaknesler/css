1939c681ba60961ac8f0250aa343cbb2e8acad41
Zak Nesler <zaknes21@yahoo.com>
2016-07-14 08:43:47 -0400

    update

---
+++ github/js/github.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('a.tabnav-tab').on('click', function () {
+        $('a.tabnav-tab').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.dropdown > a').on('click', function (e) {
+        e.preventDefault();
+
+        if (!$(this).parent('.dropdown').hasClass('open')) {
+            $('.dropdown').removeClass('open');
+            $(this).parent('.dropdown').addClass('open');
+        } else {
+            $('.dropdown').removeClass('open');
+        }
+    });
+
+    $('html').on('click', function (e) {
+        if ($(e.target).closest('.dropdown').length === 0) {
+            $('.dropdown').removeClass('open');
+        }
+    });
+
+    $(document).on('keyup', function (e) {
+        if (e.keyCode === 27) {
+            $('.dropdown').removeClass('open');
+        }
+    });
+});
